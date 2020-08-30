064af1e3b0efb5e758ecad6f4f661a8bc52ee1bc
user345 <cc@arista.com>
2020-08-30 15:27:04 +0200

    30/08/2020 - Add campaign script and updated v2 launcher

---
+++ cluster/CCF2/ccompain-testing-campaign.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+# Author : S. Cohen (August 2020)
+# Modified by : C. Compain (30/08/2020)
+# Global launcher for CCF testing campaing executions
+# Please copy this file for launching your own testing campaing by updating INPUT and OUTPUT Directory
+#
+set -x
+NUM_EXECUTOR_CORES=4
+#NUM_EXECUTORS=8
+EXECUTOR_MEMORY=4g
+EXECUTOR_MEMORY_OVERHEAD=2g
+#NUM_DRIVER_CORES=1
+DRIVER_MEMORY=4g
+#SCRIPT_NAME=$1 #CCF_DF-python2.py
+PARTITIONS=16
+HDFS_INPUT_DIR=/user/user345/input
+HDFS_INPUT_FILE=web-Google_nohead.txt
+HDFS_OUTPUT_DIR=/user/user345/output
+
+# Global campaing
+# SCRIPTS_LIST="CCF_RDD.py CCF_DF.py CCF_RDD_SS.py CCF_DF_SS.py CCF_RDD_V0.py CCF_DF_V0.py"
+# RDD Only Campaing
+SCRIPTS_LIST="CCF_RDD.py CCF_RDD_SS.py"
+# DF Only Campaing
+# SCRIPTS_LIST="CCF_DF.py CCF_DF_SS.py"
+
+for SCRIPT_NAME in $SCRIPTS_LIST
+do
+	echo "########################################"
+	echo $SCRIPT_NAME
+	for NUM_EXECUTORS in 2 4 6 8
+	do
+		for NUM_DRIVER_CORES in 1 2
+		do
+			sh launcher-v2.sh --executor-cores=$NUM_EXECUTOR_CORES --num-executors=$NUM_EXECUTORS --executor-memory=$EXECUTOR_MEMORY --executor-memory-overhead=$EXECUTOR_MEMORY_OVERHEAD --driver-cores=$NUM_DRIVER_CORES --driver-memory=$DRIVER_MEMORY --python-script=$SCRIPT_NAME --partitions=$PARTITIONS --hdfs-input-dir=$HDFS_INPUT_DIR --hdfs-input-file=$HDFS_INPUT_FILE --hdfs-output-dir=$HDFS_OUTPUT_DIR
+		done
+	done
+	echo "########################################"
+done
+exit
---
+++ cluster/CCF2/launcher-v2.sh
@@ -0,0 +1,158 @@
+#!/bin/bash
+# Author: C.Compain
+# Modified by: 
+# August 2020
+# Script for running CCF script with multiple parameters
+# Log results in $LOG_DIR
+#
+set -x
+
+# Capture input parameters 
+
+for key in "$@"
+do
+    #key="${key#*=}"
+    case "${key}" in
+        --executor-cores=*)
+        NUM_EXECUTOR_CORES=${key#*=}
+        shift
+        ;;
+        --num-executors=*)
+        NUM_EXECUTORS=${key#*=}
+        shift
+        ;;
+        --executor-memory=*)
+        EXECUTOR_MEMORY="${key#*=}"
+        shift
+        ;;
+	--executor-memory-overhead*)
+	EXECUTOR_MEMORY_OVERHEAD="${key#*=}"
+	shift
+	;;
+        --driver-cores=*)
+        NUM_DRIVER_CORES=${key#*=}
+        shift
+        ;;
+        --driver-memory=*)
+        DRIVER_MEMORY=${key#*=}
+        shift
+        ;;
+        --python-script=*)
+        PYTHON_SCRIPT=${key#*=}
+        shift
+        ;;
+        --partitions=*)
+        PARTITIONS=${key#*=}
+        shift
+        ;;
+        --hdfs-input-dir=*)
+        HDFS_INPUT_DIR=${key#*=}
+        shift
+        ;;
+        --hdfs-input-file=*)
+        HDFS_INPUT_FILE=${key#*=}
+        shift
+        ;;
+        --hdfs-output-dir=*)
+        HDFS_OUTPUT_DIR=${key#*=}
+        shift
+        ;;
+        *)
+        # Do whatever you want with extra options
+        echo "Unknown option '$key'"
+	exit 3
+        ;;
+    esac
+done
+
+# Spark cluster parameters
+CLUSTER_TYPE="yarn" 
+CLUSTER_OPTIONS="--deploy-mode cluster"
+
+# Log parameters
+LOGDIR=$HOME/backup/log
+DATE=`date +"%Y%m%d"`
+DATETIME=`date +"%Y%m%d_%H%M%S"`
+SCRIPT_NAME=`basename $0`
+JOB_NAME=$DATETIME\--$PYTHON_SCRIPT
+RAW_LOG_NAME=$JOB_NAME\--raw.log
+YARN_LOG_NAME=$JOB_NAME\--yarn.log
+RESULTS_LOG_NAME=$JOB_NAME\--results.log
+CSV_LOG_NAME=$JOB_NAME\--csv.log
+
+# Clean HDFS output directory from previous runs
+if hdfs dfs -ls $HDFS_OUTPUT_DIR;then 
+	echo "DELETE output directory"
+	hdfs dfs -rm -R $HDFS_OUTPUT_DIR
+else
+	echo "output directory OK"
+fi
+
+# spark-submit
+
+spark-submit --master $CLUSTER_TYPE $CLUSTER_OPTIONS \
+	 --executor-cores $NUM_EXECUTOR_CORES --num-executors $NUM_EXECUTORS \
+	 --executor-memory $EXECUTOR_MEMORY --conf spark.executor.memoryOverhead=$EXECUTOR_MEMORY_OVERHEAD \
+	 --conf spark.driver.cores=$NUM_DRIVER_CORES \
+	 --conf spark.driver.memory=$DRIVER_MEMORY \
+	 --conf spark.yarn.jars="file:///home/cluster/shared/vms/spark-current/jars/*.jar" \
+	 $PYTHON_SCRIPT --partition $PARTITIONS --input $HDFS_INPUT_DIR/$HDFS_INPUT_FILE --output $HDFS_OUTPUT_DIR > $LOGDIR/$RAW_LOG_NAME 2>&1
+
+retVal=$?
+
+if [ $retVal -ne 0 ]; then
+	echo "Error"
+	# Extract applicationId from the logs
+        AppId=`grep "Submitted application application_" logs.txt | cut --delimiter=' ' --fields=7`
+        # Get the yarn logs
+        yarn logs -applicationId $AppId > $LOGDIR/$YARN_LOG_NAME
+        # Extract the results
+	echo "CLUSTER_TYPE="$CLUSTER_TYPE > $LOGDIR/$RESULTS_LOG_NAME
+	echo "CLUSTER_OPTIONS="$CLUSTER_OPTIONS >> $LOGDIR/$RESULTS_LOG_NAME
+	echo "NUM_EXECUTORS="$NUM_EXECUTORS >> $LOGDIR/$RESULTS_LOG_NAME
+	echo "NUM_EXECUTOR_CORES="$NUM_EXECUTOR_CORES >> $LOGDIR/$RESULTS_LOG_NAME
+	echo "EXECUTOR_MEMORY="$EXECUTOR_MEMORY >> $LOGDIR/$RESULTS_LOG_NAME
+	echo "EXECUTOR_MEMORY_OVERHEAD="$EXECUTOR_MEMORY_OVERHEAD >> $LOGDIR/$RESULTS_LOG_NAME
+	echo "NUM_DRIVER_CORES="$NUM_DRIVER_CORES >> $LOGDIR/$RESULTS_LOG_NAME
+	echo "DRIVER_MEMORY="$DRIVER_MEMORY >> $LOGDIR/$RESULTS_LOG_NAME
+	echo "PYTHON_SCRIPT="$PYTHON_SCRIPT >> $LOGDIR/$RESULTS_LOG_NAME
+	echo "PARTITIONS="$PARTITIONS >> $LOGDIR/$RESULTS_LOG_NAME
+	echo "HDFS_INPUT_DIR="$HDFS_INPUT_DIR >> $LOGDIR/$RESULTS_LOG_NAME
+	echo "HDFS_INPUT_FILE="$HDFS_INPUT_FILE >> $LOGDIR/$RESULTS_LOG_NAME
+	echo "HDFS_OUTPUT_DIR="$HDFS_OUTPUT_DIR >> $LOGDIR/$RESULTS_LOG_NAME
+	echo "--------------------------------------------------------------------------------------"
+        grep "WARN" $LOGDIR/$YARN_LOG_NAME >> $LOGDIR/$RESULTS_LOG_NAME
+        grep "ERROR" $LOGDIR/$YARN_LOG_NAME >> $LOGDIR/$RESULTS_LOG_NAME
+else
+	echo "Success"
+	# Extract applicationId from the logs
+	AppId=`grep "Submitted application application_" $LOGDIR/$RAW_LOG_NAME | cut --delimiter=' ' --fields=7`
+	# Get the yarn logs
+	yarn logs -applicationId $AppId > $LOGDIR/$YARN_LOG_NAME
+	# Extract the results and make the log
+	echo "CLUSTER_TYPE="$CLUSTER_TYPE > $LOGDIR/$RESULTS_LOG_NAME
+	echo "CLUSTER_OPTIONS="$CLUSTER_OPTIONS >> $LOGDIR/$RESULTS_LOG_NAME
+	echo "NUM_EXECUTORS="$NUM_EXECUTORS >> $LOGDIR/$RESULTS_LOG_NAME
+	echo "NUM_EXECUTOR_CORES="$NUM_EXECUTOR_CORES >> $LOGDIR/$RESULTS_LOG_NAME
+	echo "EXECUTOR_MEMORY="$EXECUTOR_MEMORY >> $LOGDIR/$RESULTS_LOG_NAME
+	echo "EXECUTOR_MEMORY_OVERHEAD="$EXECUTOR_MEMORY_OVERHEAD >> $LOGDIR/$RESULTS_LOG_NAME
+	echo "NUM_DRIVER_CORES="$NUM_DRIVER_CORES >> $LOGDIR/$RESULTS_LOG_NAME
+	echo "DRIVER_MEMORY="$DRIVER_MEMORY >> $LOGDIR/$RESULTS_LOG_NAME
+	echo "PYTHON_SCRIPT="$PYTHON_SCRIPT >> $LOGDIR/$RESULTS_LOG_NAME
+	echo "PARTITIONS="$PARTITIONS >> $LOGDIR/$RESULTS_LOG_NAME
+	echo "HDFS_INPUT_DIR="$HDFS_INPUT_DIR >> $LOGDIR/$RESULTS_LOG_NAME
+	echo "HDFS_INPUT_FILE="$HDFS_INPUT_FILE >> $LOGDIR/$RESULTS_LOG_NAME
+	echo "HDFS_OUTPUT_DIR="$HDFS_OUTPUT_DIR >> $LOGDIR/$RESULTS_LOG_NAME
+	echo "--------------------------------------------------------------------------------------"
+	hdfs dfs -ls $HDFS_OUTPUT_DIR >> $LOGDIR/$RESULTS_LOG_NAME
+	echo "--------------------------------------------------------------------------------------"
+	grep "WARN" $LOGDIR/$YARN_LOG_NAME >> $LOGDIR/$RESULTS_LOG_NAME
+	ITERATION=`grep Iteration $LOGDIR/$RESULTS_LOG_NAME | wc -l`
+	PROCESS_TIME=`grep "Process time" $LOGDIR/$RESULTS_LOG_NAME | cut --delimiter=' ' --fields=8`
+	echo "Nb Iteration" $ITERATION
+	echo "Process time" $PROCESS_TIME
+	# Generate CSV
+	echo $JOB_NAME,$PYTHON_SCRIPT,$NUM_EXECUTOR_CORES,$NUM_EXECUTORS,$EXECUTOR_MEMORY,$EXECUTOR_MEMORY_OVERHEAD,$NUM_DRIVER_CORES,$DRIVER_MEMORY,$PROCESS_TIME,$ITERATION > $LOGDIR/$CSV_LOG_NAME
+fi
+
+
